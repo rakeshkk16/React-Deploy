7d562b0b1ab1d22df4bec7dcfe394597c5691a1a
rakesh.kumar@engineer.ai <rakesh.kumar@engineer.ai>
2021-01-06 17:21:47 +0530

    first commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,12 @@
+import './searc-box.styles.css';
+
+export const SearchBox = ({placeholder, handleChange}) => {
+    return(
+        <input 
+          className='search'
+          type='search' 
+          placeholder={placeholder} 
+          onChange={handleChange}
+        />
+    )
+}
